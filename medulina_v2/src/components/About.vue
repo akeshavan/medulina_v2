1b2677eff15de2f7e7e6e435bfeccecf772af3af
akeshavan <keshavan@berkeley.edu>
2017-11-27 10:11:05 -0800

    enh/wip: added empty about page

---
+++ medulina_v2/src/components/About.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="about mt-5 container fluid">
+    <div class="jumbotron heading">
+      <h1>About</h1>
+    </div>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+
+export default {
+  name: 'About',
+  data() {
+    return {
+      // stuff
+    };
+  },
+};
+</script>
